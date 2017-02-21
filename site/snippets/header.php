80db1a3e88bd5af766517d15dd79e0ff4da92c17
Tristan <tristan.bagot@gmail.com>
2017-02-21 14:22:45 +0100

    outdated warning

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -68,6 +68,13 @@
 </head>
 <body>
 
+<div id="outdated">
+	<div class="inner">
+	<p class="browserupgrade">You are using an <strong>outdated</strong> browser.
+	<br>Please <a href="http://outdatedbrowser.com" target="_blank">upgrade your browser</a> to improve your experience.</p>
+	</div>
+</div>
+
 <div class="loader">
 	<div class="spinner">
 		<svg class="circular" viewBox="25 25 50 50">
